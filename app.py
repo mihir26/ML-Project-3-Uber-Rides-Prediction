fa6022b039ca124c5ed648f8d49b9ec99cec33f5
Mihir <mihiragl19@gmail.com>
2021-02-20 13:14:34 +0530

    app.py modified

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import math
 app = Flask(__name__)
 model = pickle.load(open('taxi.pkl','rb'))
 
-@app.route('/',methods = ['GET'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
